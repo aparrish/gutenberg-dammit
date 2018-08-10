74f196d1f888ef7738b91b2240cf05ddabf957b9
Allison Parrish <allison@decontextualize.com>
2018-08-10 18:43:50 -0400

    well that would help

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setup(
     url='https://github.com/aparrish/gutenbergdammit',
     description='Extract plain text and metadata from the GutenTag dump',
     long_description=readme,
+    packages=setuptools.find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        'Programming Language :: Python',
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
     ],
     platforms='any',
 )
